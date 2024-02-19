fb27d68220668ca86192e131f23a24e2b5de0a0d
Gugguuuuu <148577471+Gugguuuuu@users.noreply.github.com>
2024-02-19 16:02:31 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1 className=''>Hello, world</h1>
+      </header>
+      <main>
+        <div className='esfera'>
+        <img src='terraSemFundo.png' alt='Mundo'/>
+        </div>
+      </main>
+      <footer>
+        
+O planeta Terra é o terceiro planeta do sistema solar, localizado a uma distância ideal do Sol, o que permite a existência de água líquida e uma diversidade incrível de vida. Sua atmosfera é composta principalmente por nitrogênio e oxigênio, elementos essenciais para a respiração e o desenvolvimento da vida como a conhecemos.
+
+A Terra possui uma camada externa sólida chamada de crosta, que se divide em várias placas tectônicas que se movem lentamente ao longo do tempo, causando terremotos e a formação de cadeias de montanhas. A crosta é coberta por oceanos de água salgada, que ocupam a maior parte da superfície do planeta.
+
+Além disso, a Terra possui um campo magnético que a protege das partículas carregadas do vento solar, formando a magnetosfera. Esse campo magnético é gerado no núcleo externo de ferro líquido em constante movimento, devido à rotação do planeta.
+
+A vida na Terra é incrivelmente diversa, com milhões de espécies diferentes, desde microorganismos até plantas e animais complexos. Os seres humanos, em particular, têm tido um impacto significativo no planeta, alterando a paisagem, poluindo o ar e os oceanos e causando mudanças climáticas globais.
+
+Preservar a Terra e suas diversas formas de vida é essencial para garantir um futuro sustentável para as gerações futuras.
+
+
+
+
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
